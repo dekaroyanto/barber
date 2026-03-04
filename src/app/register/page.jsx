e8fc5524dd3dad2733c9610cc0d7c421b3de48d2
Deka Royanto <dekaroy05@gmail.com>
2026-03-04 17:34:18 +0700

    [FIX]

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { signUpUser } from "@/services/auth";
+// import { signUpUser } from "@/services/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Swal from "sweetalert2";
